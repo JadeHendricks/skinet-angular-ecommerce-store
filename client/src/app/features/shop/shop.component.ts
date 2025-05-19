a2a928dce6b436921adadda8d2ec2d890f26318c
Jade Hendricks <hendricksjade@gmail.com>
2025-05-19 15:42:24 +0200

    adding in sorting, pagination and search functionality to the client side. Refactoring our store service.

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -3,29 +3,92 @@ import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/products';
 import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionListChange } from '@angular/material/list';
+import { MatSelectionList } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
-  imports: [MatCard, ProductItemComponent],
+  imports: [MatCard, ProductItemComponent, MatButton, MatIcon, MatMenu, MatSelectionList, MatListOption, MatMenuTrigger, MatPaginator, FormsModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
   // injections
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
   
-  products: Product[] = [];
+  products?: Pagination<Product>;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'},
+  ];
+  shopParams = new ShopParams();
+  pageSizeOptions = [5, 10, 15, 20];
 
   ngOnInit(): void {
     this.initializeShop();
   }
 
   private initializeShop() {
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error)
-    });
+    this.getProducts();
     this.shopService.getBrands();
     this.shopService.getTypes();
+  }
+
+  onSearchChange() {
+    this.shopParams.pageNumber = 1; // Reset to first page on search
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products = response,
+      error: error => console.log(error)
+    });
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.shopParams.pageNumber = event.pageIndex + 1;
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
+  }
+
+  onSortChange(event: MatSelectionListChange) {
+    const selectedOption = event.options[0];
+    if (selectedOption) {
+      this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1; // Reset to first page on sort change
+      this.getProducts();
+    }
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types,
+      }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (result) => {
+        if (result) {
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1; // Reset to first page on filter change
+          this.getProducts();
+        }
+      }
+    });
   }
 }
