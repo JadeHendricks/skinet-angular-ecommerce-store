4832b528468c59c4eb53e2bc71d2fe1f4c49cd85
Jade Hendricks <hendricksjade@gmail.com>
2025-05-23 14:41:24 +0200

    creating login functionality on the client side and persisting login.

---
+++ client/src/app/core/services/account.service.ts
@@ -0,0 +1,46 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { User } from '../../shared/models/user';
+import { Address } from '../../shared/models/address';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  login(values: any) {
+    let params = new HttpParams();
+    params = params.append('useCookies', true);
+    // we need to set the withCredentials to true in order to send the cookies
+    // to the server. This is because the server is on a different domain
+    // and we need to allow cross-origin requests
+    return this.http.post<User>(this.baseUrl + 'login', values, {params, withCredentials: true});
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'account/register', values);
+  }
+
+  getUserInfo(): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'account/user-info', {withCredentials: true}).pipe(
+      map(user => {
+        this.currentUser.set(user);
+        return user;
+      })
+    )
+  }
+
+  logout() {
+    return this.http.post(this.baseUrl + 'account/logout', {withCredentials: true});
+  }
+
+  updateAddress(address: Address) {
+    return this.http.post(this.baseUrl + 'account/address', address);
+  }
+}
--- client/src/app/core/services/init.service.ts
+++ client/src/app/core/services/init.service.ts
@@ -1,18 +1,24 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { Cart } from '../../shared/models/carts';
+import { AccountService } from './account.service';
+import { User } from '../../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private cartService = inject(CartService);
+  private accountService = inject(AccountService);  
 
-  init(): Observable<Cart> | Observable<null> {
+  init(): Observable<{cart: Cart | null; user: User;}> {
     const cartId = localStorage.getItem('cart_id');
     const cart$  = cartId ? this.cartService.getCart(cartId) : of(null)
 
-    return cart$;
+    return forkJoin({
+      cart: cart$,
+      user: this.accountService.getUserInfo()
+    })
   }
 }
